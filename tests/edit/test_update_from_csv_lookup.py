c6dc3bd56275ee213ec2b00ad512581fb5382d7f
Ohi Bokth <bokth89@gmail.com>
2022-03-09 15:27:53 +0000

    modified test data

--- tests/edit/test_update_from_csv_lookup.py
+++ tests/edit/test_update_from_csv_lookup.py
@@ -6,14 +6,20 @@ from cishouseholds.edit import update_from_csv_lookup
 def test_update_from_csv_lookup(spark_session, pandas_df_to_temporary_csv):
     map_df = spark_session.createDataFrame(
         data=[
-            (1, "B", "original", "converted"),
-            (2, "A", "original", "converted"),
-            (2, "B", "original", "converted"),
-            (3, "B", "original", "converted"),
-            (4, "B", "original", "converted"),
-            (5, "A", "something else", "converted"),
-            (6, "A", "value", "converted A from null"),
-            (6, "B", None, "converted B from null"),
+            # fmt: off
+            (1, "B", "original",        "converted"),
+            (2, "A", "original",        "converted"),
+            (2, "B", "original",        "converted"),
+            (3, "B", "original",        "converted"),
+            (4, "B", "original",        "converted"),
+            (5, "A", "something else",  "converted"),
+            (6, "A", "converted A from null","converted A from null"),
+            (6, "B", None,              "converted B from null"),
+
+            (7, "B", 'haven\"t',         "havent"),
+            (8, "B", "haven't",         "havent"),
+            (9, "B", "have, no",        "have not"),
+            # fmt: on
         ],
         schema="""id integer, target_column_name string, old_value string, new_value string""",
     )
@@ -25,18 +31,24 @@ def test_update_from_csv_lookup(spark_session, pandas_df_to_temporary_csv):
             (3, "original", "original"),
             (4, "original", "original"),
             (5, "original", "original"),
-            (6, "value", None),
+            (7, 'haven"t', 'haven"t'),
+            (8, "haven't", "haven't"),
+            (9, "have, no", "have, no"),
         ],
         schema="""id integer, A string, B string""",
     )
     expected_df = spark_session.createDataFrame(
         data=[
-            (1, "original", "converted"),
-            (2, "converted", "converted"),
-            (3, "original", "converted"),
-            (4, "original", "converted"),
-            (5, "original", "original"),
-            (6, "converted A from null", "converted B from null"),
+            # fmt: off
+            (1, "original",                 "converted"),
+            (2, "converted",                "converted"),
+            (3, "original",                 "converted"),
+            (4, "original",                 "converted"),
+            (5, "original",                 "original"),
+            (7, 'haven"t',                  "havent"),
+            (8, "haven't",                  "havent"),
+            (9, "have, no",                 "have not"),
+            # fmt: on
         ],
         schema="""id integer, A string, B string""",
     )
